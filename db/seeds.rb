337bcb1990f556768283979968a204706451e255
samda <muy.samda13@gmail.com>
2016-09-24 14:21:42 +0700

    setup database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.where(title: "setup react").first_or_create(content: 'do research about setting up react js.', done: false)
